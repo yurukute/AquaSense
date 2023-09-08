52761e6e6b5f98640ef2632a7300ac71fbaf37a0
yurukute <nguyenkhanhdung.9999@gmail.com>
2023-09-08 23:57:19 +0700

    Setup maxtrix310 env
    - Set default env.
    - Set common options for all environments.
    - Install Artila-Maxtrix310 lib.
    
    Branch: matrix310

---
+++ include/Artila-Matrix310.h
@@ -0,0 +1,42 @@
+/*
+ *   (C) Artila Electronics Co., Ltd. All Rights Reserved.
+ *       Date: 2022.06.10
+ */
+
+#define LIB_VERSION "0.1.0"
+
+// LED
+#define LED_READY   27
+#define LED_WIFI    21
+
+//LAN
+#define LAN_SCLK    18
+#define LAN_MISO    19
+#define LAN_MOSI    23
+#define LAN_CS      5
+#define LAN_RST     22
+
+// SD Card
+#define SD_MISO     12
+#define SD_MOSI     13
+#define SD_SCK      14
+#define SD_CS       15
+
+// DIO
+#define DO1         25
+#define DI1         36
+#define DI2         39
+
+// Switch
+#define SW_1        34
+#define SW_2        26
+#define SW_4        35
+
+// RS-485
+#define COM1_RX     16 //out
+#define COM1_TX     17 //in
+#define COM1_RTS    4  //request to send
+
+// RS-232
+#define COM2_RX     33
+#define COM2_TX     32
